4562c30fa613ca528fd56416b60e611572ccf2d8
Shashank <shashankkrj@gmail.com>
2023-05-04 00:49:00 +0530

    Final Commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import pandas as pd
 import streamlit as st
 import pickle
+from matplotlib import pyplot as plt
 from PIL import Image
 from google_images_search import GoogleImagesSearch
 
@@ -11,15 +12,16 @@ features = pickle.load(open('features.pkl','rb'))
 label = pickle.load(open('label.pkl','rb'))
 predictor1 = pickle.load(open('dtree.pkl','rb'))
 predictor2 = pickle.load(open('rf.pkl','rb'))
+Brand = pd.read_csv('brand.csv')
 
 def predict(input_data):
     predicted_price = np.round(predictor2.predict(input_data)[0],2)
     st.write('Predicted Price is: $ {}'.format(predicted_price))
 
 # Define a function to fetch the image of the smartwatch from Google search
-def fetch_smartwatch_image(brand, component, number = 12, model = ''):
+def fetch_smartwatch_image(brand, component, number = 11, model = ''):
     query = f'{brand} {model} {component}'
-    gis = GoogleImagesSearch('AIzaSyABwpVf6SxK1UD8T8enCEXLUqJ0Q-wym58','d07e24259591a4e69')
+    gis = GoogleImagesSearch('AIzaSyCmRvJKwNjCYBTpGEtx7MOT-V3UHgg4JqY','d07e24259591a4e69')
     gis.search({'q': query,'num': number})
     images_url = []
     for result in gis.results():
@@ -32,7 +34,7 @@ def picture_adjuster(images_url, i):
     img_data = np.frombuffer(response.content, np.uint8)
     img = cv2.imdecode(img_data, cv2.IMREAD_COLOR)
     if type(img) == type(None) :
-        image = picture_adjuster(images_url, i+6)
+        image = picture_adjuster(images_url, i + 5)
     else:
         # Resize image to 300x300
         img_resized = cv2.resize(img, (300, 300))
@@ -42,6 +44,19 @@ def picture_adjuster(images_url, i):
         cv2.imwrite('image_with_border.jpg', img_with_border)
         image = Image.open('image_with_border.jpg')
     return image
+
+def plot_chart(brand_name):
+    for i in list(Brand.index):
+            if Brand["Brand"][i] == brand:
+                Brand.loc[i, "No. of Searches"] = Brand.loc[i, "No. of Searches"] + 1
+    
+    fig, ax = plt.subplots()
+    sorted_brand = Brand.sort_values(by = "No. of Searches", ascending= False).head(6)
+    ax.bar(sorted_brand["Brand"], sorted_brand["No. of Searches"] , color ='blue',width = 0.5)
+    plt.xlabel("Brand Name")
+    plt.ylabel("No. of Searches")
+    st.pyplot(fig)
+    Brand.to_csv('brand.csv')
 
 st.title('SMART WATCH PRICE PREDICTION')
 col1, col2, col3 = st.columns(3)
@@ -120,7 +135,6 @@ for i in input_data.select_dtypes(include = ['object']).columns:
     input_data[i][0] = label[input_data[i][0]]
 
 if st.button('PREDICT'):
-    print(input_data)
     predict(input_data)
     st.subheader('Suggestion For Watches Matching Above Features :-')
     if len(model) > 0:
@@ -129,7 +143,6 @@ if st.button('PREDICT'):
         images_url = fetch_smartwatch_image(brand,'smartwatch', model = i)
         col1, col2, col3 = st.columns(3)
         with col1 :
-            
             st.image(picture_adjuster(images_url, 0), use_column_width=True)
             st.image(picture_adjuster(images_url, 3), use_column_width=True)
         with col2 :
@@ -145,3 +158,6 @@ if st.button('PREDICT'):
             st.write(" ")
             st.image(images_url, use_column_width=True)
         st.write("We have no smartwatch details in our database that match the defined features for the {} brand.".format(brand))
+
+    st.subheader('Search Trends :-')
+    plot_chart(brand_name= brand)
